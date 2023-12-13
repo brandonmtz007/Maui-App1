c0bcb25ded4f0a8478f3843b37664b744cec4fd9
brandon <brandonmtz179@gmail.com>
2023-12-13 17:21:58 -0600

    Add project files.

---
+++ parte1uno/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace parte1uno
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
